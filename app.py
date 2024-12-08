80e88031fb65dc81cdd8099f8326c45aa4db33e5
LUISG47 <jl.galindez@live.com>
2024-12-07 22:25:00 -0600

    C07C01
    C07C01

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
+session = Session(engine)
+
 #################################################
 # Flask Setup
 #################################################
@@ -45,9 +47,18 @@ def home():
                 <li>/api/v1.0/precipitation</li>
                 <li>/api/v1.0/stations</li>
                 <li>/api/v1.0/tobs</li>
-                <li>/api/v1.0/&lt;start&gt;</li>
-                <li>/api/v1.0/&lt;start&gt;/&lt;end&gt;</li>
+                <li>/api/v1.0/startdate <strong>(in YYYY-MM-DD format)</strong></li>
+                <li>/api/v1.0/startenddate <strong>(in YYYY-MM-DD/YYYY-MM-DD format)</strong></li>
               </ul>'''
+
+###################################################################################################################
+#                                                PRECIPITATION
+#
+#Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) 
+# to a dictionary using date as the key and prcp as the value.
+#Return the JSON representation of your dictionary. 
+###################################################################################################################
+
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
@@ -70,6 +81,11 @@ def precipitation():
 
     return jsonify(precipitation_dict)
 
+###################################################################################################################
+#                                                STATION
+#
+#Return a JSON list of stations from the dataset. 
+###################################################################################################################
 
 @app.route('/api/v1.0/stations')
 def stations():
@@ -84,6 +100,15 @@ def stations():
     stations_list = [station[0] for station in results]
 
     return jsonify(stations_list)
+
+
+###################################################################################################################
+#                                                TABS
+#
+# Query the dates and temperature observations of the most-active station for the previous year of data.
+# Return a JSON list of temperature observations for the previous year.
+# 
+###################################################################################################################
 
 @app.route('/api/v1.0/tobs')
 def tobs():
@@ -108,6 +133,21 @@ def tobs():
     tobs_list = [temp[0] for temp in results]
 
     return jsonify(tobs_list)
+
+
+###################################################################################################################
+#                                           <start> <start>/<end>
+#
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a 
+# specified start or start-end range.
+# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+# For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to 
+# the end date, inclusive.
+# 
+###################################################################################################################
+
+
+
 
 @app.route('/api/v1.0/<start>')
 def start(start):
